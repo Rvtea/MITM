dc6b95985350ac5776a1bd8e5aa78e36cab840b7
Chuanye Wang <chuanyewang@freewheel.tv>
2018-02-27 18:17:51 +0800

    dev

--- popup.js
+++ popup.js
@@ -3,6 +3,29 @@
 var enableStatus;
 
 $(document).ready(function() {
+    const $saveBtn = $('#saveBtn');
+    const $fileInput = $('#replaceFilePath');
+    const $urlRegInput = $('#urlReg');
+    const $fileNameLink = $('#fileName');
+    const $resetBtn = $('#resetBtn');
+
+    $urlRegInput.val(localStorage['url_regexp']);
+    $fileNameLink.html(localStorage['local_js_file_name']);
+    $fileNameLink.attr('href', localStorage.js_content);
+    $fileNameLink.on('click', e => {
+        e.preventDefault();
+        chrome.tabs.create({url: localStorage.js_content, active: true});
+    });
+
+    const reloadPage = () => {
+      chrome.tabs.query({active: true, currentWindow: true}, tabs => {
+        chrome.tabs.reload(tabs[0].id, {
+          bypassCache: true // skip cache is needed here
+        });
+        window.close();
+      });
+    };
+
     // use null to get all the k-v pairs
     chrome.storage.local.get(null, function(item) {
         console.log("Now we have initial value as: " + item["extensionEnable"]);
@@ -25,5 +48,34 @@ $(document).ready(function() {
         chrome.storage.local.set({"extensionEnable": enableStatus}, function() {
             console.log('Save successfully!');
         });
+    });
+
+    $saveBtn.on('click', () => {
+        const file = $fileInput[0].files[0];
+        let fileName = file ? file.name : '';
+        localStorage.local_js_file_name = fileName;
+
+        const regexp = $urlRegInput.val();
+        localStorage.url_regexp = regexp;
+
+        if (file) {
+          const reader = new FileReader();
+          reader.onload = e => {
+            const fileContent = e.target.result;
+            localStorage.js_content = fileContent;
+            reloadPage();
+          };
+          reader.readAsDataURL(file);
+        } else {
+          reloadPage();
+        }
+    });
+
+    $resetBtn.on('click', () => {
+      localStorage.clear();
+      $urlRegInput.val('');
+      $fileNameLink.html('');
+      $fileNameLink.attr('href', '#nogo');
+      reloadPage();
     });
 });
